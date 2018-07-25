930d1a3cf1e73fcbb521718d0f3ec434a0f1aa31
Craig Taub <craigtaub@gmail.com>
2018-07-25 11:51:21 +0100

    use grid

--- bitwise/decode.js
+++ bitwise/decode.js
@@ -3,18 +3,34 @@ var slimResult = 'opmfeo';
 
 var puzzleEncoded = parseInt(slimResult, 36); // base 36 decode
 
-getX = (v) => v % 8;
-getY = (v) => v >> 3; // Math.floor(p / 8).
-
 var positions = [];
 for(i=0;i<32;i++) { // max js 32-bit int
-if ((puzzleEncoded & 1<<i) === 1<<i) {
+  if ((puzzleEncoded & 1<<i) === 1<<i) {
     positions.push(i);
   }
 }
 
-// Index-Grid positions
-positions.forEach(index => {
-  console.log(`Index=${index}, X=${getX(index)}, Y=${getY(index)}`);
-});
+// // X/Y coords of grid
+// getX = (v) => v % 8;
+// getY = (v) => v >> 3; // Math.floor(p / 8).
+// // Index-Grid positions
+// positions.forEach(index => {
+//   console.log(`Index=${index}, X=${getX(index)}, Y=${getY(index)}`);
+// });
 
+// draw grid, not 0-indexed
+var line = '';
+for(i=1;i<=32;i++) {
+  var exists = positions.some(item => {
+    return (item+1) === i
+  });
+  if (exists) {
+    line += 'X'
+  } else {
+    line += '-'
+  }
+  if (i % 8 === 0) {
+    console.log(line)
+    line = '';
+  }
+}
--- bitwise/encode.js
+++ bitwise/encode.js
@@ -1,12 +1,12 @@
 /* GRID
 - 32 position (8x4) central grid (32-bit max js integer).
 - grid can be ANY size, centre is 8x4
---------
+- e.g (0 indexed)
+-------X
 ----X---
 ----X---
---------
+X--XX-X-
 */
-
 var blackSquare = 7, blackSquareA = 12, blackSquareB = 20, 
     blackSquareC = 24, blackSquareD = 27, blackSquareE = 28, blackSquareF = 30;
 
@@ -14,7 +14,6 @@ var blackSquare = 7, blackSquareA = 12, blackSquareB = 20,
 // each index:
 //  - is an x/y position.
 //  - is an exponent of 2 (i.e. index 3 is 2*2*2) i.e. TWO's COMPLIMENT
-// NOTE cant store numbers like 12 or 20 as made up of other flags so will confuse things
 // NOTE above 32 and starts losing accuracy (e.g. adds 3)
 
 var puzzleEncoded = 1 << blackSquare | 1 << blackSquareA | 1 << blackSquareB | 
